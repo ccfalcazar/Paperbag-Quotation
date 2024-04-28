97ebae79debe2b60b72e0dd5b371979607c11758
Christian <ccfalcazar@gmail.com>
2024-04-28 22:15:33 +0800

    Update Form.tsx

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -5,13 +5,13 @@ function Form()
 {
     return (
         <div className='vh-100 d-flex row'>
-            <div className='col-md-8 card m-auto shadow border border-5 border-light'>
-                <div className='card-header pb-3 m-0'>
+            <div className='col-md-8 card m-auto shadow border border-5 border-light m-0 p-0'>
+                <div className='card-header py-3 m-0 px-2'>
                     <div className='row p-0 m-0 text-center'>
-                    <div className='col-md-4'>  
+                    <div className='col-md-2'>  
                         <Logo/>
                     </div>
-                    <div className='col-md-8 mt-auto'>
+                    <div className='col-md-10'>
                         <p className='display-6 fs-3 text-primary text-md-end pt-4 mb-0'>Paper Bag Quotation Form</p>
                     </div>
                     </div>
