33160419df47ee4e48a6c668df2dd6cf97b5d076
Christian <ccfalcazar@gmail.com>
2024-05-16 08:40:39 +0800

    UpdateComputation

---
+++ src/components/InputFields/InputField.tsx
@@ -0,0 +1,49 @@
+
+interface Props {
+    item: string,
+    textType: string
+    inputValue: any,
+    inputEvent: (event: any)=>void;
+}
+
+function InputField({item,textType,inputValue,inputEvent}: Props)
+{
+    function InputType()
+    {
+    let Material = ['Ordinary','Kraft Brown', 'Premium'];
+    let Lamination = ['None', 'Matte', 'Plastic']
+
+    if(item == 'Material')
+        {
+            return <>
+            <select id={item} onChange={inputEvent} className="form-select text-center" >
+                {Material.map((mats)=>(
+                    <option key={mats}>{mats}</option>
+                ))}
+                
+            </select>
+            </>
+        }
+    else if(item == 'Lamination')
+        {
+            return <select onChange={inputEvent} className="form-select text-center">
+            {Lamination.map((lam)=>(
+                <option key={lam}>{lam}</option>
+            ))}
+            
+        </select>
+        }
+    else
+        return <input id={item} type={textType} value={inputValue} onChange={inputEvent} className="form-control text-center"></input>
+    }
+    
+return (
+<>
+<div key={"div" + item} className="input-group mb-2">
+    <span className="input-group-text w-50">{item}:</span>
+    {InputType()}
+</div>
+</>
+);
+}
+export default InputField;
--- src/components/Requirements/Requirements.tsx
+++ src/components/Requirements/Requirements.tsx
@@ -1,71 +1,126 @@
-
-function InputType(item:string, TextType:string)
-{
-    let Material = ['Kraft Brown', 'Ordinary', 'Premium'];
-    let Lamination = ['None', 'Matte', 'Plastic']
-
-    if(item == 'Material')
-        {
-            return <>
-            <select key={"sel" + item} id={item} className="form-select text-center" >
-                {Material.map((mats)=>(
-                    <option key={mats}>{mats}</option>
-                ))}
-                
-            </select>
-            </>
-        }
-    else if(item == 'Lamination')
-        {
-            return <select key={"lam" + item} className="form-select text-center">
-            {Lamination.map((lam)=>(
-                <option key={lam}>{lam}</option>
-            ))}
-            
-        </select>
-        }
-    else
-        return <input key={"txt"+item} id={item} type={TextType} className="form-control text-center"></input>
-}
+import { useState} from "react";
+import InputField from "../InputFields/InputField";
 
 function Requirements()
 {
-    let ClientDetails=['Name','Company Name', 'Contact Number','Email']
-    let Req = ['Quantity','Length', 'Width','Height','Material','Print Colors','Lamination'];
+    const [UnitPrice, ComputeUnitPrice] = useState(FormatCurrency(0));
+    const [TotalPrice, ComputeTotalPrice] = useState(FormatCurrency(0));
+    const [Quantity, SetQuantity] = useState(0);
+    const [Depth, SetDepth] = useState(0);
+    const [Width, SetWidth] = useState(0);
+    const [Height, SetHeight] = useState(0);
+    const [MaterialSelected, SetMaterial] = useState(0);
+    const [ColorNumber, SetColors] = useState(0);
+    const [LaminationSelected, SetLamination] = useState(0);
+    const [CustomerName, SetCustomerName] = useState('');
+    const [CompanyName, SetCompanyName] = useState('');
+    const [ContactNumber, SetContactNumber] = useState('');
+    const [CustomerEmail, SetCustomerEmail] = useState('');
+    const Size = function(PaperLength : number, PaperWidth : number) 
+    {
+        const Length = PaperLength;
+        const Width = PaperWidth;
+        return {Length, Width};
+    }
+    
+    function handlesQuantity(e : any)
+    {
+        SetQuantity(e.target.value);
+    }
+    function handlesLength(e:any)
+    {
+        SetDepth(e.target.value);
+    }
+    function handlesWidth(e:any)
+    {
+        SetWidth(e.target.value);
+    }
+    function handlesHeight(e:any)
+    {
+        SetHeight(e.target.value);
+    }
+    function handlesMaterial(e:any)
+    {
+        SetMaterial(e.target.value);
+    }
+    function handlesColor(e:any)
+    {
+        SetColors(e.target.value);
+    }
+
+    function handlesLamination(e:any)
+    {
+        SetLamination(e.target.value);
+    }
+    function handlesCustomerName(e:any)
+    {
+        SetCustomerName(e.target.value);
+    }
+    function handlesCompanyName(e:any)
+    {
+        SetCompanyName(e.target.value);
+    }
+    function handlesContactNumber(e:any)
+    {
+        SetContactNumber(e.target.value);
+    }
+    function handlesCustomerEmail(e:any)
+    {
+        SetCustomerEmail(e.target.value);
+    }
+
+    function FormatCurrency(number: number | bigint)
+    {
+        return new Intl.NumberFormat('en-US',{style:"currency", currency:'PHP'}).format(number);
+    }
+
+    function ComputePaperBagSize()
+    {
+        let TotalRunningLength = (parseFloat(Depth.toString())/2 + 0.5) + parseFloat(Height.toString());
+        let TotalRunningWidth = parseFloat(Width.toString()) + parseFloat(Depth.toString()) + 0.5;
+
+        const PaperSize = Size(TotalRunningLength,TotalRunningWidth);
+        alert(PaperSize.Length);
+    }
+
+    function Compute()
+    {
+        ComputePaperBagSize();
+        ComputeUnitPrice(FormatCurrency(Quantity));
+        ComputeTotalPrice(FormatCurrency(1000));
+    }
+
     return (
         <>
         <div className="row">
             <div className="col-md-6">
                 <form>
                 <h5 className="mb-3">Paperbag Details:</h5>
-                {
-                    Req.map((item)=>(
-                    <div key={"div" + item} className="input-group mb-2">
-                            <span key={"span" + item} className="input-group-text w-50">{item}:</span>
-                            {InputType(item,"number")}
-                    </div>
-                ))}
-                <div className="btn btn-outline-primary w-100 mb-2">Submit Details</div>
+                    <InputField item={"Quantity (pcs)"} inputValue={Quantity} inputEvent={handlesQuantity} textType={'number'}></InputField>
+                    <InputField item={"Width (inches)"} inputValue={Width} inputEvent={handlesWidth} textType={'number'}></InputField>
+                    <InputField item={"Depth (inches)"} inputValue={Depth} inputEvent={handlesLength} textType={'number'}></InputField>
+                    <InputField item={"Height (inches)"} inputValue={Height} inputEvent={handlesHeight} textType={'number'}></InputField>
+                    <InputField item={"Print Colors"} inputValue={ColorNumber} inputEvent={handlesColor} textType={'number'}></InputField>
+                    <InputField item={"Material"} inputValue={MaterialSelected} inputEvent={handlesMaterial} textType={'number'}></InputField>
+                    <InputField item={"Lamination"} inputValue={LaminationSelected} inputEvent={handlesLamination} textType={'number'}></InputField>
+                <div className="btn btn-outline-primary w-100 mb-2" id="btnSubmit" onClick={Compute}>Submit Details</div>
                 <div className="input-group mb-2">
                     <span className="input-group-text w-50">Unit Price</span>
-                    <input type="number" className="form-control text-center" disabled key="UnitPrice"></input>
+                    <input type="text" className="form-control text-center" disabled key="UnitPrice" id="txtUnitPrice" value={UnitPrice}></input>
                 </div>
                 <div className="input-group mb-2">
                     <span className="input-group-text w-50">Total Price</span>
-                    <input type="number" className="form-control text-center" disabled key="TotalPrice"></input>
+                    <input type="text" className="form-control text-center" disabled key="TotalPrice" id="txtTotalPrice" value={TotalPrice}></input>
                 </div>
                 </form>
             </div>
             <div className="col-md-6 justify-content-center">
                 <form>
                 <h5 className="mb-3">Customer Details:</h5>
-                {
-                    ClientDetails.map((item)=>(
-                    <div key={"div" + item} className="input-group mb-3">
-                            <span key={"span"+item} className="input-group-text w-50">{item}:</span>
-                            {InputType(item,"text")}
-                    </div>
-                ))}
+                    <InputField item="Name" inputValue={CustomerName} textType="text" inputEvent={handlesCustomerName}/>
+                    <InputField item="Company Name" inputValue={CompanyName} textType="text" inputEvent={handlesCompanyName}/>
+                    <InputField item="Contact Number" inputValue={ContactNumber} textType="text" inputEvent={handlesContactNumber}/>
+                    <InputField item="Email" inputValue={CustomerEmail} textType="text" inputEvent={handlesCustomerEmail}/>
                 <span className="d-flex justify-content-center"><button className="btn btn-outline-primary">Send Quotation</button></span>
                 </form>
             </div>
